55a65c0c491e9a48ddbb2f610229633078ee2a6c
andywar65 <andy.war1965@gmail.com>
2019-03-05 09:57:31 +0100

    getting rid of unused models

--- models.py
+++ models.py
@@ -120,33 +120,6 @@ class MaterialPageComponent(Orderable):
         #FieldPanel('weight'),
     ]
 
-class MaterialIndexPage(Page):
-    introduction = models.TextField(
-        help_text='Text to describe the page',
-        blank=True)
-
-    content_panels = Page.content_panels + [
-        FieldPanel('introduction', classname="full"),
-    ]
-
-    # Speficies that only MaterialPage objects can live under this index page
-    subpage_types = ['MaterialPage']
-
-    # Defines a method to access the children of the page (e.g. MaterialPage
-    # objects).
-    def children(self):
-        return self.get_children().specific().live()
-
-    # Overrides the context to list all child items, that are live, by the
-    # date that they were published
-    # http://docs.wagtail.io/en/latest/getting_started/tutorial.html#overriding-context
-    def get_context(self, request):
-        context = super(MaterialIndexPage, self).get_context(request)
-        context['posts'] = MaterialPage.objects.descendant_of(
-            self).live().order_by(
-            '-first_published_at')
-        return context
-
 class ScenePage(Page):
     introduction = models.CharField(max_length=250, null=True, blank=True,
     help_text="Scene description",)
@@ -209,7 +182,7 @@ class ScenePage(Page):
         ], heading="Presentation", classname="collapsible collapsed"),
         MultiFieldPanel([
             DocumentChooserPanel('dxf_file'),
-            InlinePanel('cad_files', label="Other CAD file/s",),
+            #InlinePanel('cad_files', label="Other CAD file/s",),
             FieldPanel('shadows'),
             FieldPanel('fly_camera'),
             FieldPanel('double_face'),
@@ -258,8 +231,10 @@ class ScenePageLayer(Orderable):
     name = models.CharField(max_length=250, default="0",
         help_text="As in CAD file",)
     invisible = models.BooleanField(default=False, help_text="Hide layer?",)
-    wireframe = models.BooleanField(default=False, help_text="Display only wire frame?",)
-    no_shadows = models.BooleanField(default=False, help_text="Layer casts no shadows?",)
+    wireframe = models.BooleanField(default=False,
+        help_text="Display only wire frame?",)
+    no_shadows = models.BooleanField(default=False,
+        help_text="Layer casts no shadows?",)
     material = models.ForeignKey(MaterialPage, blank=True, null=True,
         on_delete=models.SET_NULL)
     partition = models.ForeignKey(PartitionPage, blank=True, null=True,
@@ -272,30 +247,6 @@ class ScenePageLayer(Orderable):
         FieldPanel('no_shadows'),
         FieldPanel('material'),
         FieldPanel('partition'),
-    ]
-
-class ScenePageCadFile(Orderable):
-    page = ParentalKey(ScenePage, related_name='cad_files')
-    dxf_file = models.ForeignKey(
-        'wagtaildocs.Document',
-        null=True,
-        on_delete = models.SET_NULL,
-        related_name = '+',
-        help_text="Additional CAD files of your project",
-        )
-    x_position = models.FloatField(default="0",
-        help_text="In meters, displacement with respect to axis origin",)
-    y_position = models.FloatField(default="0", help_text="In meters, see above",)
-    z_position = models.FloatField(default="0", help_text="In meters, see above",)
-    rotation = models.FloatField(default="0",
-        help_text="In degrees, counterclockwise around Z axis",)
-
-    panels = [
-        DocumentChooserPanel('dxf_file'),
-        FieldPanel('x_position'),
-        FieldPanel('y_position'),
-        FieldPanel('z_position'),
-        FieldPanel('rotation'),
     ]
 
 class SceneIndexPage(Page):
@@ -324,55 +275,6 @@ class SceneIndexPage(Page):
             self).live().order_by(
             '-first_published_at')
         return context
-
-class ArScenePage(Page):
-    introduction = models.CharField(max_length=250, null=True, blank=True,
-    help_text="Scene description",)
-    image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
-    )
-    date_published = models.DateField(
-        "Date article published", blank=True, null=True
-        )
-    author = models.ForeignKey(User, blank=True, null=True,
-        on_delete=models.PROTECT)
-    scene = models.ForeignKey(ScenePage, blank=True, null=True,
-        on_delete=models.PROTECT, help_text="Choose Scene you'd like to see in AR")
-
-    search_fields = Page.search_fields + [
-        index.SearchField('introduction'),
-    ]
-
-    content_panels = Page.content_panels + [
-        MultiFieldPanel([
-            FieldPanel('introduction'),
-            ImageChooserPanel('image'),
-            FieldPanel('author'),
-            FieldPanel('date_published'),
-        ], heading="Presentation", classname="collapsible collapsed"),
-        FieldPanel('scene'),
-    ]
-
-    def add_new_layers(self):
-        self.scene.path_to_dxf = os.path.join(settings.MEDIA_ROOT, 'documents', self.scene.dxf_file.filename)
-        add_new_layers_ext(self.scene)
-        return
-
-    def get_object_assets(self):
-        object_dict = aframe.get_object_dict(self.scene)
-        return object_dict
-
-    def get_material_assets(self):
-        return get_material_assets_ext(self.scene)
-
-    def get_entities(self):
-        mode = 'ar'
-        return get_entities_ext(self.scene, mode)
 
 class DigkomPage(Page):
     introduction = models.CharField(max_length=250, null=True, blank=True,
---
+++ models_backup.py
@@ -0,0 +1,99 @@
+class MaterialIndexPage(Page):
+    introduction = models.TextField(
+        help_text='Text to describe the page',
+        blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('introduction', classname="full"),
+    ]
+
+    # Speficies that only MaterialPage objects can live under this index page
+    subpage_types = ['MaterialPage']
+
+    # Defines a method to access the children of the page (e.g. MaterialPage
+    # objects).
+    def children(self):
+        return self.get_children().specific().live()
+
+    # Overrides the context to list all child items, that are live, by the
+    # date that they were published
+    # http://docs.wagtail.io/en/latest/getting_started/tutorial.html#overriding-context
+    def get_context(self, request):
+        context = super(MaterialIndexPage, self).get_context(request)
+        context['posts'] = MaterialPage.objects.descendant_of(
+            self).live().order_by(
+            '-first_published_at')
+        return context
+
+class ScenePageCadFile(Orderable):
+    page = ParentalKey(ScenePage, related_name='cad_files')
+    dxf_file = models.ForeignKey(
+        'wagtaildocs.Document',
+        null=True,
+        on_delete = models.SET_NULL,
+        related_name = '+',
+        help_text="Additional CAD files of your project",
+        )
+    x_position = models.FloatField(default="0",
+        help_text="In meters, displacement with respect to axis origin",)
+    y_position = models.FloatField(default="0", help_text="In meters, see above",)
+    z_position = models.FloatField(default="0", help_text="In meters, see above",)
+    rotation = models.FloatField(default="0",
+        help_text="In degrees, counterclockwise around Z axis",)
+
+    panels = [
+        DocumentChooserPanel('dxf_file'),
+        FieldPanel('x_position'),
+        FieldPanel('y_position'),
+        FieldPanel('z_position'),
+        FieldPanel('rotation'),
+    ]
+
+class ArScenePage(Page):
+    introduction = models.CharField(max_length=250, null=True, blank=True,
+    help_text="Scene description",)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+    )
+    date_published = models.DateField(
+        "Date article published", blank=True, null=True
+        )
+    author = models.ForeignKey(User, blank=True, null=True,
+        on_delete=models.PROTECT)
+    scene = models.ForeignKey(ScenePage, blank=True, null=True,
+        on_delete=models.PROTECT, help_text="Choose Scene you'd like to see in AR")
+
+    search_fields = Page.search_fields + [
+        index.SearchField('introduction'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('introduction'),
+            ImageChooserPanel('image'),
+            FieldPanel('author'),
+            FieldPanel('date_published'),
+        ], heading="Presentation", classname="collapsible collapsed"),
+        FieldPanel('scene'),
+    ]
+
+    def add_new_layers(self):
+        self.scene.path_to_dxf = os.path.join(settings.MEDIA_ROOT, 'documents', self.scene.dxf_file.filename)
+        add_new_layers_ext(self.scene)
+        return
+
+    def get_object_assets(self):
+        object_dict = aframe.get_object_dict(self.scene)
+        return object_dict
+
+    def get_material_assets(self):
+        return get_material_assets_ext(self.scene)
+
+    def get_entities(self):
+        mode = 'ar'
+        return get_entities_ext(self.scene, mode)
